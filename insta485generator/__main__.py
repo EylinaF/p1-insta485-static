e3c81caf6038110d0408afd7048c3b1c6244dbde
eylina <eylinaf@umich.edu>
2025-01-13 15:59:15 -0500

    pass html code

---
+++ insta485generator/__main__.py
@@ -0,0 +1,14 @@
+"""Build static HTML site from directory of HTML templates and plain files."""
+import click
+import pathlib
+from pathlib import Path
+
+@click.command()
+@click.argument("input_dir", nargs=1, type=click.Path(exists=True))
+def main(input_dir):
+    input_dir = pathlib.Path(input_dir)
+    print(f"DEBUG input_dir={input_dir}")
+
+
+if __name__ == "__main__":
+    main()
